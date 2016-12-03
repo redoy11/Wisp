70939edfa62f7b96c690ae34618a32e7373ccd41
redoy11 <redoyahmed11@gmail.com>
2016-12-03 12:03:38 +0600

    Splash screen is done differently. It is now done as a theme rather than a layout

--- app/src/main/java/com/company/wisp/wisp/MainActivity.java
+++ app/src/main/java/com/company/wisp/wisp/MainActivity.java
@@ -40,13 +40,14 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
     void do_splash()
     {
+        /*
         new Handler().postDelayed(new Runnable() {
 
             /*
              * Showing splash screen with a timer. This will be useful when you
              * want to show case your app logo / company
              */
-
+/*
             @Override
             public void run() {
                 // This method will be executed once the timer is over
@@ -58,6 +59,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 finish();
             }
         }, SPLASH_TIME_OUT);
+    */
+
+        Intent i = new Intent(MainActivity.this, MenuActivity.class);
+        startActivity(i);
     }
     void signIn()
     {
